3a7b78f4cea988a418f07530f794d6b625c5ef2d
Edgar Olivar <edgar.olivar@skyairline.com>
2020-06-08 21:52:00 -0400

    container first

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 let sidebar = document.getElementById("sidebar");
+let container = document.getElementById("container")
 
 const openSidebar = () => {
+  let containerWidth = document.body.clientWidth - 250
   sidebar.style.display = "block";
   document.body.style.overflow = "hidden"
-
+  container.style.width = containerWidth + "px"
 };
 
 const closeSidebar = () => {
+  let containerWidth = document.body.clientWidth
   sidebar.style.display = "none";
   document.body.style.overflow = "auto"
+  // container.style.width = container + "px"
 };
 
 if(document.body.clientWidth > 900){
